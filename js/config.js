5528edfd8dd69fa0dfcf97285f79f25fd55ec0fa
james341 <taine049@gmail.com>
2021-07-02 18:28:02 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -161,6 +161,7 @@ var l_musicDisplay = true;
 var l_musicPlaylist = [
 	{ogg: "killerinp.ogg", name: "Nations - Installation Music"},
 	{ogg: "blueprint.ogg", name: "Nations - Installation Music"},
+	{ogg: "earthwind.ogg", name: "Nations - Installation Music"},
 	{ogg: "blindinglights.ogg", name: "Nations - Installation Music"},
 	{ogg: "dsafyouspinme.ogg", name: "Nations - Installation Music"},
 	{ogg: "fredbearfight.ogg", name: "Nations - Installation Music"},
