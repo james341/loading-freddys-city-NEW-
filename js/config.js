226554c4086a390438ae838d4fb339bc5c8fc0f6
james341 <taine049@gmail.com>
2021-07-09 21:14:08 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -159,7 +159,7 @@ var l_musicDisplay = true;
 // (Place the ogg files in the music folder)
 // (You can also use external URL for the ogg files)
 var l_musicPlaylist = [
-	{ogg: "killerinp.ogg", name: "Nations - Installation Music"},
+	{ogg: "pineradio.ogg", name: "Nations - Installation Music"},
 	{ogg: "pineradiotwo.ogg", name: "Nations - Installation Music"},
 	{ogg: "xmanback.ogg", name: "Nations - Installation Music"},
 ];
