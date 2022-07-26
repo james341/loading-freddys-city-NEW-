1d8c5f22b377d54493cf51a965add4da372902cb
james341 <taine049@gmail.com>
2022-07-26 17:07:52 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -162,7 +162,7 @@ var l_musicDisplay = true;
 // (Place the ogg files in the music folder)
 // (You can also use external URL for the ogg files)
 var l_musicPlaylist = [
-	{ogg: "superIdolsng.ogg", name: "Nations - Installation Music"},
+	{ogg: "comeandgetyourlove.ogg", name: "Nations - Installation Music"},
 	{ogg: "TitleScreen.ogg", name: "Nations - Installation Music"},
 	{ogg: "superidoldsafmusicbox.ogg", name: "Nations - Installation Music"},
 	{ogg: "daycarethememusicbox.ogg", name: "Nations - Installation Music"},
