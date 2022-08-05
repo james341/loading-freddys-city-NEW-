7977335ac41929350410c498278b270c26cd8d66
james341 <taine049@gmail.com>
2022-08-05 14:53:19 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -164,6 +164,7 @@ var l_musicDisplay = true;
 var l_musicPlaylist = [
 	{ogg: "comeandgetyourlove.ogg", name: "Nations - Installation Music"},
 	{ogg: "forgottensundayshow.ogg", name: "Nations - Installation Music"},
+	{ogg: "flipsidedsaf.ogg", name: "Nations - Installation Music"},
 	{ogg: "salvagemusic.ogg", name: "Nations - Installation Music"},
 	{ogg: "daycarethememusicbox.ogg", name: "Nations - Installation Music"},
 	{ogg: "dsafyeasttwonine.ogg", name: "Nations - Installation Music"},
