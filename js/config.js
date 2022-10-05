5dcbb8872cebebd241907be4da07e1b4e5f00d35
james341 <taine049@gmail.com>
2022-10-05 17:07:23 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -165,6 +165,7 @@ var l_musicDisplay = true;
 var l_musicPlaylist = [
 	{ogg: "moonlianaflores.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "stopmedsaf.ogg", name: "【AN】 - Installation Music"},
+	{ogg: "dsafyouspinme.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "sneighbourhood.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "dsafyeasttwonine.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "smokindsaf.ogg", name: "【AN】 - Installation Music"},
