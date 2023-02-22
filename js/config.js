b8eec3e6d42d3982ec66163e8e9d6f0245c7efa4
james341 <taine049@gmail.com>
2023-02-23 06:16:43 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -174,11 +174,9 @@ var l_musicDisplay = true;
 // (You can also use external URL for the ogg files)
 var l_musicPlaylist = [
 	{ogg: "moonlianaflores.ogg", name: "【AN】 - Installation Music"},
-	{ogg: "dsafclownandjokers.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "superidoldsafmusicbox.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "imbeliever.ogg", name: "【AN】 - Installation Music"},
-	{ogg: "neighbourhood.ogg", name: "【AN】 - Installation Music"},
-	{ogg: "securitybreachelevatorthree.ogg", name: "【AN】 - Installation Music"},
+	{ogg: "twobirdsregina.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "behindtheslaughteremix.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "smokindsaf.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "dsafsuperidol.ogg", name: "【AN】 - Installation Music"},
