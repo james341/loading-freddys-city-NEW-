6afd4b6c8b5d92d0f4b8de7457bb793b3cfc2cd6
james341 <taine049@gmail.com>
2023-04-17 23:44:13 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -178,6 +178,7 @@ var l_musicDisplay = true;
 var l_musicPlaylist = [
 	{ogg: "moonlianaflores.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "dsafclownandjokers.ogg", name: "【AN】 - Installation Music"},
+	{ogg: "incrediblesagain.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "cailloulilboom.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "daycarethememusicbox.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "UncleKrackerMusicBox.ogg", name: "【AN】 - Installation Music"},
