387f1657a0221b74907f7291c95e3da1e4b63d28
james341 <taine049@gmail.com>
2023-08-29 16:28:25 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -187,13 +187,12 @@ var l_musicDisplay = true;
 // (You can also use an external URL for the Ogg files)
 var l_musicPlaylist = [
 	{ogg: "moonlianaflores.ogg", name: "【AN】 - Installation Music"},
-	{ogg: "billyjoel.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "athephoneharrygoodnight.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "youfeelthelove.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "flipsidedsaf.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "twobirdsregina.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "sleeplessdavetheme.ogg", name: "【AN】 - Installation Music"},
-	{ogg: "dsafyouspinme.ogg", name: "【AN】 - Installation Music"},  
+	{ogg: "daycarethememusicbox.ogg", name: "【AN】 - Installation Music"},  
 ];
 
 // Random music order?
