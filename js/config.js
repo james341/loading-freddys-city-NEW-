451be6641bbb69ed184a669822058d0992effbe8
james341 <taine049@gmail.com>
2024-03-09 18:56:00 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -194,7 +194,7 @@ var l_musicPlaylist = [
 	{ogg: "absofacto.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "ghostbusterdsaf.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "neighbourhood.ogg", name: "【AN】 - Installation Music"},
-	{ogg: "dsafbirthday.ogg", name: "【AN】 - Installation Music"},
+	{ogg: "tuckedup.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "daycarethememusicbox.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "youfeelthelove.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "flipsidedsaf.ogg", name: "【AN】 - Installation Music"},
