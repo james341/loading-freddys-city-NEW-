cf8b91aea48144f485fb77aff03dda1a18595bd0
james341 <taine049@gmail.com>
2024-05-10 22:15:47 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -198,6 +198,7 @@ var l_musicDisplay = true;
 // (You can also use an external URL for the Ogg files)
 var l_musicPlaylist = [
 	{ogg: "moonlianaflores.ogg", name: "【AN】 - Installation Music"},
+	{ogg: "mamapapabrickolinimusic.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "dsafrealgoodending.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "twobirdsregina.ogg", name: "【AN】 - Installation Music"},
 	{ogg: "dsafweaselpop.ogg", name: "【AN】 - Installation Music"},
