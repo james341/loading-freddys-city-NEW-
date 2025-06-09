1b2405e8f89bdccf3b2225c989490850734dbf71
james341 <taine049@gmail.com>
2025-06-09 13:44:37 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -27,7 +27,7 @@ var l_serverName = "sleekLoad 2";
 // Use a video for the background?
 // Otherwise it will use images
 // 'true' or 'false'
-var l_bgVideo = true;
+var l_bgVideo = false;
 
 // Default available videos:
 // "aurora.webm"
@@ -55,6 +55,7 @@ var l_bgVideo = true;
 // Just make sure they are using webm VP8 format 
 // (Place the videos in the backgrounds/videos folder)
 // (You can also use external URL)
+var l_background = "freddyrp.webm";
 var l_background = "freddyrp.webm";
 
 // (Image-background only)
