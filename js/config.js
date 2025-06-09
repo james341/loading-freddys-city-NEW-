1f883f891fdbba94560c75f40fa8a4e89bfa6b42
james341 <taine049@gmail.com>
2025-06-09 13:20:37 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -217,7 +217,7 @@ var l_musicVolume = 15;
 
 // Enable custom messages?
 // 'true' or 'true'
-var l_messagesEnabled = true;
+var l_messagesEnabled = false;
 
 // Enter your custom messages below
 var l_messages = [
